a5a592eb94797a2a9ea8180a771ec05d39fdff26
fz <super9192010@gmail.com>
2014-03-05 11:13:48 +0800

    modified python3.py file

--- python3.py
+++ python3.py
@@ -2,7 +2,7 @@
 import sys
 type = sys.getdefaultencoding()
 print 'sysdefaultencoding is %s' % type
-a = '哈哈'.decode('utf-8').encode('gbk')
+a = '哈哈'.decode('utf-8').encode('gb2312')
 b = a
 a = '2014-03-05'
 print 'Hi, your word is %s , and today is %s' % (b, a)
