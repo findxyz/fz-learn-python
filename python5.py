8effd6fde453f1930de69514add0bacd47903cf4
fz@zzwh <super9192010@gmail.com>
2014-03-08 11:02:12 +0800

    python5 practice set

--- python5.py
+++ python5.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 dict = {"fz": 26, "xx": 27}
 dict["cc"] = 28
 print dict["fz"]
@@ -6,3 +7,31 @@ print dict.get("bb", "not found")
 print dict
 print dict.pop("cc")
 print dict
+
+#key = [1, 2, 3]
+#dict[key] = "1 2 3"
+
+s = set([1, 2, 3, 1])
+print s
+s.add(4)
+print s
+s.remove(1)
+print "s:", s
+
+s2 = set([3, 4, 5])
+print 's2:', s2
+
+s3 = s & s2
+print "s3=s & s2:", s3
+
+s4 = s | s2
+print "s4=s | s2:", s4
+
+tuple = (1, 2, 3)
+tuple2 = (1, [2, 3])
+
+s.add(tuple)
+print "s.add(1, 2, 3):", s
+s.add(tuple2)
+print "s.add(1, [2, 3]):", s
+
